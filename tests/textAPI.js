856c97bb1ccc5b810db3d74413a70ecfbcb7bbf2
Beth Marshall <beth.marshall@askherconsulting.co.uk>
2020-09-13 22:15:48 +0100

    fixed wait for navigation test

--- tests/textAPI.js
+++ tests/textAPI.js
@@ -1,7 +1,8 @@
-var _selectors = require('./selectors')
-var assert = require('assert')
+/*jshint esversion: 8 */
+var _selectors = require('./selectors');
+var assert = require('assert');
 
 step("length of parse int should be <elementLength> <table>", async function (elementLength, table) {
-    var elementText = await _selectors.getElement(table).text()
-	assert.ok(elementText.length>=elementLength)
+    var elementText = await _selectors.getElement(table).text();
+    assert.ok(elementText.length<=elementLength);
 });
